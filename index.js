b462dec9813bf163026731ddf245079c049104ca
Melanie Penn <melanie.penn17@gmail.com>
2022-10-08 02:26:30 -0500

    commit tree d1a314313c696b6c791f4a28ff2c8778acffd129

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+    return cats
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+    return cats
+};
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+    return cats
+};
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+    return cats
+};
+
+function appendCat(name) {
+    return [...cats, name]
+};
+
+function prependCat(name) {
+    return [name, ...cats]
+};
+
+function removeLastCat(name) {
+    return cats.slice(0, cats.length - 1)
+};
+
+function removeFirstCat(name) {
+    return cats.slice(1)
+};
